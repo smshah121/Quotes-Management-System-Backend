15c17cf47af3f46e7868011486ff81fb674d0b09
smshah121 <smshah.2003@gmail.com>
2025-06-04 19:09:50 +0500

    make some crypto changes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { webcrypto } from 'node:crypto';
+import { ConfigService } from '@nestjs/config';
 
 // Polyfill for crypto.randomUUID
 if (!globalThis.crypto) {
@@ -13,6 +14,9 @@ if (!globalThis.crypto) {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService); // <--- ADD THIS LINE
+  const jwtSecret = configService.get<string>('JWT_SECRET'); // <--- ADD THIS LINE
+  console.log('DEBUG: JWT_SECRET from ConfigService:', jwtSecret); // 
   
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
