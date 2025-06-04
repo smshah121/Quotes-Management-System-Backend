b14a3a4376b5690c236325bd792f51fe2d80c453
smshah121 <smshah.2003@gmail.com>
2025-06-04 18:44:49 +0500

    make some crypto changes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { webcrypto } from 'crypto';
+import { webcrypto } from 'node:crypto';
+
+// Polyfill for crypto.randomUUID
+if (!globalThis.crypto) {
+  globalThis.crypto = {
+    randomUUID: () => webcrypto.randomUUID()
+  } as any;
+}
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
