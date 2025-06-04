53625d07bf569c3b3e392499397f2489238ff452
smshah121 <smshah.2003@gmail.com>
2025-06-04 17:24:45 +0500

    add crypto in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { webcrypto } from 'crypto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto as any;
+}
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
     origin: "*"
