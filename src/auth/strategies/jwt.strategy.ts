69a29bd01bb46214752f1f1b36572c78979176f7
smshah121 <smshah.2003@gmail.com>
2025-09-02 01:00:27 +0500

    changes saved

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -7,6 +7,7 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     const jwtSecret = configService.get<string>('JWT_SECRET');
+    console.log('JWT_SECRET:', jwtSecret);
     
     if (!jwtSecret) {
       throw new Error('JWT_SECRET is not defined in environment variables');
