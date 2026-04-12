436dd2a5971d178a077b42fb6e822887912a5ec6
smshah121 <smshah.2003@gmail.com>
2026-04-12 14:10:03 +0500

    cs

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService
+
+  ) {
     const jwtSecret = configService.get<string>('JWT_SECRET');
     if (!jwtSecret) throw new Error('JWT_SECRET is not defined');
 
@@ -16,7 +21,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return { id: payload.sub, email: payload.email };
-  }
+  async validate(payload:any){
+        const user = await this.authService.validateUserById(payload.sub)
+        if(!user) throw new UnauthorizedException()
+        return {
+        ...user,
+        sub:payload.sub
+        }
+    }
 }
